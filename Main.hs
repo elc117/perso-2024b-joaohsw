05a834fa38de3d468584fb7be0ca5708d1f51d7e
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2024-10-20 20:47:50 +0000

    Refactor code structure and add quiz functionality

---
+++ Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Web.Scotty
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy.IO as TIO
+
+main :: IO ()
+main = scotty 3000 $ do
+  middleware logStdoutDev
+
+  get "/home" $ do
+    file "static/home.html"
+
+  get "/quiz" $ do
+    file "static/quiz.html"
+
+  get "/static/:file" $ do
+    fileName <- param "file"
+    let filePath = "static/" ++ fileName
+    file filePath
+
+  post "/submit" $ do
+    answer <- param "answer"
+    if answer == ("Brasília" :: Text)
+      then html "<h2>Resposta Correta!</h2><a href=\"/quiz\">Voltar ao quiz</a>"
+      else html "<h2>Resposta Incorreta.</h2><a href=\"/quiz\">Tentar de novo</a>"
