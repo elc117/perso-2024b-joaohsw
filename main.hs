76f3241261764aff2f6ae101c62f4cd1437085be
João Henrique Scherer Wolski <142943270+joaohsw@users.noreply.github.com>
2024-10-20 19:35:46 +0000

    Deleted: .devcontainer/Dockerfile Modified: .devcontainer/devcontainer.json New file: LICENSE Modified: main.hs New file: perso-2024b-joaohsw.cabal

--- main.hs
+++ main.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import qualified Data.Text.Lazy as T  -- Importa o módulo para manipular 'Text'
 
 main :: IO ()
 main = scotty 3000 $ do
   middleware logStdoutDev
 
-  -- Define your routes and handlers here
+  -- Define your routes and handlers aqui
   get "/hello" $ do
-    text "Quiz de geografia"
-    text "Qual a capital do Brasil?"
-    text "a) São Paulo"
-    text "b) Rio de Janeiro"
-    text "c) Brasília"
-    text "d) Salvador"
+    text (T.pack "Quiz de geografia")  -- Converte a string para 'Text'
+    text (T.pack "Qual a capital do Brasil?")
+    text (T.pack "a) São Paulo")
+    text (T.pack "b) Rio de Janeiro")
+    text (T.pack "c) Brasília")
+    text (T.pack "d) Salvador")
